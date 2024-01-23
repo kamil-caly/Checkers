0aee2ef884942fe612022039f0550a1ce9b4c306
Kamil Cały <kamil.caly@vp.pl>
2024-01-23 17:58:05 +0100

    draw board

---
+++ checkers_solution/project_GUI/AssetsLoader.cs
@@ -0,0 +1,29 @@
+﻿using project_logic;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace project_GUI
+{
+    public static class AssetsLoader
+    {
+        private const string pawnBlackUrl = "assets/PawnB.png";
+        private const string pawnWhiteUrl = "assets/PawnW.png";
+        private const string LadyBlackUrl = "assets/QueenB.png";
+        private const string LadyWhiteUrl = "assets/QueenW.png";
+
+        private readonly static Dictionary<FieldContent, string> whitePieces = new()
+        {
+            { FieldContent.Pawn, pawnWhiteUrl },
+            { FieldContent.Lady, LadyWhiteUrl }
+        };
+
+        private readonly static Dictionary<FieldContent, string> blackPieces = new()
+        {
+            { FieldContent.Pawn, pawnBlackUrl },
+            { FieldContent.Lady, LadyBlackUrl }
+        };
+
+        public static ImageSource GetImage(FieldContent content, Player color) =>
+            new BitmapImage(new Uri(color == Player.White ? whitePieces[content] : blackPieces[content], UriKind.Relative));
+    }
+}
--- checkers_solution/project_GUI/MainWindow.xaml.cs
+++ checkers_solution/project_GUI/MainWindow.xaml.cs
@@ -17,10 +17,33 @@ namespace project_GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int _rows = 8;
+        private const int _cols = 8;
+        private readonly GameState _gameState;
         public MainWindow()
-        {
-            GameState gameState = new GameState();  
+        { 
             InitializeComponent();
+            _gameState = new GameState();
+            DrawBoard();
+        }
+
+        private void DrawBoard()
+        {
+            for (int r = 0; r < _rows; r++)
+            {
+                for (int c = 0; c < _cols; c++)
+                {
+                    Image img = new Image();
+                    PieceGrid.Children.Add(img);
+
+                    BoardField boardField = _gameState.GetBoardField(r, c);
+                    if (boardField.Player == null)
+                        continue;
+
+                    ImageSource imgSource = AssetsLoader.GetImage(boardField.Content, (Player)boardField.Player);
+                    img.Source = imgSource;
+                }
+            }
         }
     }
 }
